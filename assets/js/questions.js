f621a1a23acb2e3646ecc63667b6b9a6afd3c4c6
Xiao <xiao@xiaos-iMac.local>
2023-01-21 16:30:33 +0000

    quiz questions added to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,122 @@
+var myQuestions = [
+    {
+        question: "Why would you surround a piece of text with <h1></h1> tags?",
+        answers: {
+            a: "to indicate that this text is the main heading on the page",
+            b: "to make the text bold",
+            c: "to ensure that search engines treat the keywords as being important for this page",
+            d: "to demonstrate to screen readers that this text is important"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "When might an empty alt attribute be the correct value?",
+        answers: {
+            a: "when you cannot think of useful alt text",
+            b: "when you don't think it would be interesting to someone who cannot see it",
+            c: "when the image has come from a CMS",
+            d: "when the image is purely decorative"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which attribute must have a unique value each time it is used in an HTML document",
+        answers: {
+            a: "title",
+            b: "class",
+            c: "style",
+            d: "id"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which CSS property will not trigger layout recalculation?",
+        answers: {
+            a: "top",
+            b: "opacity",
+            c: "width",
+            d: "height"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What does the === comparison operator do?",
+        answers: {
+            a: "It sets one variable equal to another in both value and type",
+            b: "It tests for equality of type only",
+            c: "It tests for equality of value only",
+            d: "It tests for equality of value and type"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which choice is not a value of the type attribute of the <input> element?",
+        answers: {
+            a: "range",
+            b: "address",
+            c: "date",
+            d: "password"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which choice is not part of CSS box model?",
+        answers: {
+            a: "margin",
+            b: "border",
+            c: "paddding",
+            d: "paragraph"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which part of the URL https://app.uniswap.org/pool specifies the domain name?",
+        answers: {
+            a: "https",
+            b: "org",
+            c: "uniswap.org",
+            d: "app.uniswap"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which HTML element is not considered a landmark element?",
+        answers: {
+            a: "<form>",
+            b: "<ul>",
+            c: "<main>",
+            d: "<nav>"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which statement is true when an HTML tag has been deprecated?",
+        answers: {
+            a: "It employs code that can be viewed only on a desktop computer",
+            b: "It is obsolete and is not recommended for use in marking web content",
+            c: "It employs code that will require users to update their browsers",
+            d: "It employs incorrect syntax that will cause the browser to crash"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which array method should you apply to run a function for every item within an array, returning an array of all items for which the function is true?",
+        answers: {
+            a: "every()",
+            b: "map()",
+            c: "forEach()",
+            d: "filter()"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "You want to create striped table rows using CSS without adding a class to any element. Which CSS would correctly apply the background color to every odd row in your table?",
+        answers: {
+            a: "tr:nth-child(2) { background-color: #ccc; }",
+            b: "r:nth-child(2n+1) { background-color: #ccc; }",
+            c: "tr:nth-child(2+1) { background-color: #ccc; }",
+            d: "tr:nth-child(2n) { background-color: #ccc; }"
+        },
+        correctAnswer: "b"
+    }
+]
